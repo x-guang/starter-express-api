2bdb40e5ffe959813e9306aa3e77a82c4a5b9a34
xguang <42057530+x-guang@users.noreply.github.com>
2023-11-04 21:53:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,11 +6,21 @@ app.all('/', (req, res) => {
 })
 app.all('/t', async(req, res) => {
     console.log("Just got a test request!")
+    for(let i=0; i<11;i++){
+        res.write("i:"+i+"<br>\n")
+        await sleep(1e3)
+    }
+    res.write("end!")
+    res.end()
+})
+app.all('/t1', async(req, res) => {
+    console.log("Just got a test request!")
     for(let i=0; i<1;i++){
         res.write("i:"+i+"<br>\n")
         await sleep(1e3)
     }
-    res.send('End!')
+    res.write("end!")
+    res.end()
 })
 app.listen(process.env.PORT || 3000)
 
