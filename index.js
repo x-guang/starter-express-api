1d5fd3d559e3c03867fe963db659bdbe6496d655
xguang <42057530+x-guang@users.noreply.github.com>
2023-11-04 21:44:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,16 @@ app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
+app.all('/t', async(req, res) => {
+    console.log("Just got a test request!")
+    for(let i=0; i<11;i++){
+        res.write("i:"+i+"<br>\n")
+        await sleep(1e3)
+    }
+    res.send('End!')
+})
 app.listen(process.env.PORT || 3000)
+
+function sleep(timeout) {
+  return new Promise((res) => setTimeout(res, timeout))
+}
